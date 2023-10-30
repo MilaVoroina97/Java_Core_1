dc2da2a2938fd143a82836268e734f7777f72620
mila <milavoronina1997@mail.ru>
2023-10-30 21:59:44 +0500

    correct find difference class

--- src/main/java/homework/two/FindDifference.java
+++ src/main/java/homework/two/FindDifference.java
@@ -6,23 +6,23 @@ public class FindDifference {
      * Написать функцию, возвращающую разницу между самым большим и самым ма- леньким элементами
      * переданного не пустого массива.
      *
-     * @param nums Принимает массив
+     * @param numbers Принимает массив
      * @return Возвращаем разницу между максимальным и минимальным элементами
      */
-    public static int findDifference(int[] nums) {
-        if (nums.length <= 1) {
+    public static int findDifference(int[] numbers) {
+        if (numbers.length <= 1) {
             return 0; // или другое значение по умолчанию
         }
 
-        int max = nums[0];
-        int min = nums[0];
+        int max = numbers[0];
+        int min = numbers[0];
 
-        for (int num : nums) {
-            if (num > max) {
-                max = num;
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
             }
-            if (num < min) {
-                min = num;
+            if (number < min) {
+                min = number;
             }
         }
 
