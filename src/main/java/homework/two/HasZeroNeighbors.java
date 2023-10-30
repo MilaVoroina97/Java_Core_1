254a8140cbbc20c4818ec7ba314201529d16fce3
mila <milavoronina1997@mail.ru>
2023-10-30 22:01:16 +0500

    correct neighbors class

--- src/main/java/homework/two/HasZeroNeighbors.java
+++ src/main/java/homework/two/HasZeroNeighbors.java
@@ -6,16 +6,16 @@ public class HasZeroNeighbors {
      * Написать функцию, возвращающую истину, если в переданном массиве есть два соседних элемента, с
      * нулевым значением.
      *
-     * @param nums Принимает массив
+     * @param numbers Принимает массив
      * @return Возвращает true или false
      */
-    public static boolean hasZeroNeighbors(int[] nums) {
-        if (nums.length < 2) {
+    public static boolean hasZeroNeighbors(int[] numbers) {
+        if (numbers.length < 2) {
             return false;
         }
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == 0 && nums[i + 1] == 0) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == 0 && numbers[i + 1] == 0) {
 
                 return true;
             }
