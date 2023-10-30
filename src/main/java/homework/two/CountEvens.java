5905807fd098ca941217c1206c4f9ad6fabf5be7
mila <milavoronina1997@mail.ru>
2023-10-30 21:58:37 +0500

    correct count evens class

--- src/main/java/homework/two/CountEvens.java
+++ src/main/java/homework/two/CountEvens.java
@@ -5,13 +5,13 @@ public class CountEvens {
     /**
      * Написать метод, возвращающий количество чётных элементов массива. countEvens([2, 1, 2, 3, 4]) →
      * 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
-     * @param nums Принимает массив
+     * @param numbers Принимает массив
      * @return возвращает количество чётных элементов массива.
      */
-    public static int countEvens(int[] nums) {
+    public static int countEvens(int[] numbers) {
         int count = 0;
-        for (int num : nums) {
-            if (num % 2 == 0) {
+        for (int number : numbers) {
+            if (number % 2 == 0) {
                 count++;
             }
         }
