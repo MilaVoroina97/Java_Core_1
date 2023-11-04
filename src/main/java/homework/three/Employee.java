4661074f632a928c5993d12d428be44e517128ee
mila <milavoronina1997@mail.ru>
2023-11-04 11:10:40 +0500

    create method date comparator

--- src/main/java/homework/three/Employee.java
+++ src/main/java/homework/three/Employee.java
@@ -1,5 +1,9 @@
 package homework.three;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Employee {
 
     private String name;
@@ -41,6 +45,35 @@ public class Employee {
 
     public void increaseSalary(double amount) {
         salary += amount;
+    }
+
+    public void dateComparator(String d1, String d2){
+
+
+        String pattern = "MM-dd-yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+
+        Date firstDate = null;
+        Date secondDate = null;
+
+        try {
+            firstDate = sdf.parse(d1);
+            secondDate = sdf.parse(d2);
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        int ret = firstDate.compareTo(secondDate);
+        if (ret > 0) {
+            System.out.printf("%s is after %s", d1, d2);
+        }
+        else if (ret < 0) {
+            System.out.printf("%s is before %s", d1, d2);
+        }
+        else {
+            System.out.print("Both dates are equal");
+        }
     }
 
     @Override
