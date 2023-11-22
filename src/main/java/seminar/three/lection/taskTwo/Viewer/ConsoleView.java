4736f2de72190c7950bd4e6bb94a08c73bd3b006
mila <milavoronina1997@mail.ru>
2023-11-23 00:48:28 +0500

    add menu

--- src/main/java/seminar/three/lection/taskTwo/Viewer/ConsoleView.java
+++ src/main/java/seminar/three/lection/taskTwo/Viewer/ConsoleView.java
@@ -16,14 +16,11 @@ public class ConsoleView implements View<Animal>{
     public double getUserInput() throws NumberFormatException {
         while (true){
             System.out.println("Enter a double number.");
-            if(scanner.hasNextDouble()) {
-                try {
-                    return Double.parseDouble(scanner.next());
-                } catch (NumberFormatException ne) {
-                    System.out.println("Input is not a number, continue");
-                }
+            try {
+                return Double.parseDouble(scanner.next());
+            } catch (NumberFormatException ne) {
+                System.out.println("Input is not a number, continue");
             }
-
         }
     }
 
