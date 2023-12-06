d2cf16b051f3f95443c0f193d27eddd185cf8b9b
mila <milavoronina1997@mail.ru>
2023-12-06 20:32:28 +0500

    finish lections tasks

--- src/main/java/seminar/four/taskThree/SumOfNumbersInSquareArray.java
+++ src/main/java/seminar/four/taskThree/SumOfNumbersInSquareArray.java
@@ -1,14 +1,21 @@
 package seminar.four.taskThree;
 
+
+/**
+ *  Для проверки напишите программу, преобразующую квадратный массив целых
+ * чисел 5х5 в сумму чисел в этом массиве, при этом, программа должна выбросить
+ * исключение, если строк или столбцов в исходном массиве окажется не 5.
+ */
+
 public class SumOfNumbersInSquareArray {
 
     public static void main(String[] args) {
 
         int[][] array = new int[4][5];
-        sumOfNumbers(array);
+        System.out.println(sumOfNumbers(array));
     }
 
-    public static int sumOfNumbers(int array[][]){
+    public static int sumOfNumbers(int[][] array){
         int sum = 0;
         if(array[0].length == 5 && array.length == 5){
             for(int i = 0; i < array.length; i++){
