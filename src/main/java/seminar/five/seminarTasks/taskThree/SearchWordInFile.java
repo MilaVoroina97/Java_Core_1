4ed7ae0fccf227a4d0653e52b9c7040b74ef8e2d
mila <milavoronina1997@mail.ru>
2023-12-24 22:12:48 +0500

    hm done

--- src/main/java/seminar/five/seminarTasks/taskThree/SearchWordInFile.java
+++ src/main/java/seminar/five/seminarTasks/taskThree/SearchWordInFile.java
@@ -1,42 +1,43 @@
 package seminar.five.seminarTasks.taskThree;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SearchWordInFile {
 
     public static void main(String[] args) throws IOException {
-        searchWordInFile("the-file-name.txt","The");
+        searchWordInFile("the-file-name.txt", "The");
     }
 
     public static void searchWordInFile(String path, String wordToFind) throws IOException, FileNotFoundException {
 
         File file = new File(path);
-        List<String> saveAllLinesForRewriting = new ArrayList<String>();
+        String[] words = null;
         String savedLine;
-        int count = 0;
-        if(!file.exists())
+        String lineNumber = "";
+        int count = 0, countBuffer = 0, countLine = 0;
+        if (!file.exists())
             throw new FileNotFoundException("File not found");
         try(BufferedReader reader = new BufferedReader(new FileReader(file))){
-            while ((savedLine = reader.readLine()) != null){
-                saveAllLinesForRewriting.add(savedLine);
-            }
-            for(String word : saveAllLinesForRewriting){
-                if(word.equals(wordToFind))
-                    count++;
-            }
-            if (count != 0) { // Check for count not equal to zero
-                System.out.println("The given word: " + wordToFind + " is present for " + count
-                        + " times in the file");
-            } else {
-                System.out.println("The given word is not present in the file");
+            while ((savedLine = reader.readLine())!= null){
+                countLine++;
+                words = savedLine.split(" ");
+                for(String word : words){
+                    if(word.equals(wordToFind)){
+                        count++;
+                        countBuffer++;
+                    }
+                }if(countBuffer > 0){
+                    countBuffer = 0;
+                    lineNumber += countLine + ",";
+
+                }
             }
 
         }catch (IOException ex){
             System.out.println(ex.getMessage());
         }
 
-
+        System.out.println("Times found at--"+count);
+        System.out.println("Word found at--"+lineNumber);
     }
 }
